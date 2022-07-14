e7a5e350edbdf955b9fe3074bc229f6a21ba019e
William Nonato <williamnonato9@gmail.com>
2022-07-14 14:48:02 +0200

    Uploaded and verified in the goerli testnet

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project is the contract related section to the dHealth dApp. 
 
+Here is the verified contract address for easy look up on [goerli.etherscan.io](https://goerli.etherscan.io/) :
+
+* *0x4bC6836a35e0C22579c89B5e31e0eaB2645a516b*
+
 ### Acknowledgements
 
 Final Project for 6th trimester of studies for Web Development at SAE Institute Belgrade.
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config({path:__dirname+'/.env'})
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
     solidity: "0.8.9",
     networks: {
         hardhat: {
             chainId: 31337
+        },
+        goerli:{
+            url: API_URL,
+            accounts: [`0x${PRIVATE_KEY}`]
         }
     }
 };

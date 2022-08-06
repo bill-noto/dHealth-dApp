1b30283f9c0e0dd3ee04d6292eb6d393e1e407f9
William Nonato <williamnonato9@gmail.com>
2022-08-06 21:22:35 +0200

    code cleanup

--- fp-dhealth-vue/src/views/HomeView.vue
+++ fp-dhealth-vue/src/views/HomeView.vue
@@ -14,43 +14,23 @@
     <div class="xl:w-4/5 xl:mx-auto">
       <div class="text-center mb-10 mx-8">
         <h1 class="text-2xl font-bold">Welcome, account {{ this.currentAccount }}</h1>
-
-<!--        <form action="" class="mx-auto inline-block text-sm w-2/3 my-10">-->
-<!--          <div>-->
-<!--            <input v-model="this.target_id" type="text" id="who" name="who" class="px-1.5 mx-auto w-2/3 text-center sm:block mb-3 border border-black" placeholder="Search by ID">-->
-<!--          </div>-->
-<!--          <button @click="getUser" id="calculate" type="button"-->
-<!--                  class="mx-auto block transform transition-all hover:bg-gray-200 hover:scale-125 text-xl border border-black w-1/3 sm:w-1/3">-->
-<!--            Search-->
-<!--          </button>-->
-<!--        </form>-->
-
       </div>
     </div>
-
-<!--    <UserComponent :who="this.who"/>-->
-
   </div>
 </template>
 
 <script>
 import Users from '../../../fp-dhealth-hardhat/artifacts/contracts/Users.sol/Users.json';
 import {ethers} from "ethers";
-// import UserComponent from "@/components/UserComponent";
 
 export default {
   name: 'HomeView',
-  // components: {
-  //   // UserComponent
-  // },
   data() {
     return {
       currentAccount: null,
       currentContract: null,
       contractAddress:
           "0x4419C04C75BC89f8858d19eb8df39957c2134fE2",
-      // target_id: null,
-      // who: {}
     };
   },
   methods: {
@@ -105,17 +85,7 @@ export default {
       if (this.currentContract != null) {
         console.log("It worked!");
       }
-    },
-
-    // getUser: async function () {
-    //   await this.currentContract.getUser(this.target_id);
-    //
-    //   this.currentContract.on("GetUser", (event) => {
-    //     // console.log(user);
-    //     console.log(event);
-    //   })
-    // }
-
+    }
   },
   mounted() {
     this.checkIfWalletIsConnected();
